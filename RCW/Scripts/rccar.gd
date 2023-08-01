f61c4e18a3988484c748398fecd832db15219e53
AutisticRobot <tmkormann04@gmail.com>
2023-07-31 21:42:12 -0400

    got the RC car working

--- RCW/Scripts/rccar.gd
+++ RCW/Scripts/rccar.gd
@@ -1,1 +1,5 @@
 extends VehicleBody3D
+
+func _physics_process(delta):
+    steering = lerp(steering, Input.get_axis("rit","lft") * 0.4, 5 * delta)
+    engine_force = Input.get_axis("dwn","up") * 100
