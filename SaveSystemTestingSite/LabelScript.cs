ba0db16caacd2f9a79b023a3bd4dcb58c7d96ffa
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2024-02-16 21:25:21 -0800

    worked on learning the godot save system

--- SaveSystemTestingSite/LabelScript.cs
+++ SaveSystemTestingSite/LabelScript.cs
@@ -5,14 +5,29 @@ public partial class LabelScript : Label
 {
 	[Export] public int count;
 	[Export] public int intId;
+
+	private main man;
+	private int loadState = 0; //checks with main script to see if it should load or just wait;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		man = GetParent<main>();
+
+		if(!man.data.ContainsKey(intId))
+		{
+			man.data.Add(intId, count);
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if(man.loadState < loadState)
+		{
+			count = (int)man.data[intId];// it is posible, with save editing, to have the data entry not exist;
+			loadState++;
+		}
+
 		Text = count.ToString();
 	}
 
--- SaveSystemTestingSite/main.cs
+++ SaveSystemTestingSite/main.cs
@@ -1,40 +1,69 @@
 using Godot;
 using Godot.Collections;
 using System;
+using System.IO;
 
 public partial class main : Node2D
 {
+	[Export] string filePath;
+	[Export] string fileName;
+
+	public int loadState = 0; //checks with main script to see if it should load or just wait;
 	public Dictionary data = new();
+	private string loadedData;
+
+
+	string globalPath;
+	string dirPath;
+	string path;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		data.Add("label0", 0);
-		data.Add("text", "Text");
-
-		Dictionary test = new()
-        {
-            { "testData", 5 }
-        };
-
-		data.Add("data", test);
-
-		string json = Json.Stringify(data);
-
-		GD.Print(json);
+		globalPath = ProjectSettings.GlobalizePath("user://");
+	
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
 
 	public void Save()
 	{
+		dirPath = Path.Join(globalPath, filePath);
+		path = Path.Join(dirPath, fileName);
+
+		if(!Directory.Exists(dirPath))
+		{
+			Directory.CreateDirectory(dirPath);
+		}
+
+		GD.Print(path);
+		string json = Json.Stringify(data);
+
+		try
+		{
+			File.WriteAllText(path, json);
+		}
+		catch (System.Exception e)
+		{
+			GD.Print(e);
+		}
 
 	}
 	public void Load()
 	{
+		dirPath = Path.Join(globalPath, filePath);
+		path = Path.Join(dirPath, fileName);
 
+		if(!File.Exists(path)) return;
+
+		try
+		{
+			loadedData = File.ReadAllText(path);
+		}
+		catch (System.Exception e)
+		{
+			GD.Print(e);
+		}
+
+		loadState++;
 	}
 }
